928c50c4837579366e4d548f2482dac7e0472e7d
Roman Burger <roman.arnold.burger@gmail.com>
2012-02-23 15:49:26 +0100

    Using clippy

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -31,6 +31,9 @@ class PagesController < ApplicationController
     sa mas
   }
 }"
+    @text = "<div xmlns=\"http://www.w3.org/1999/xhtml\" class=\"simplesect\"><div class=\"titlepage\"><hr/></div> <p> </p><pre class=\"programlisting\">#include &lt;stdio.h&gt; typedef struct { unsigned den : 5; /* bity 0 - 4 */ unsigned mesic : 4; /* bity 5 - 8 */ unsigned rok : 7; /* bity 9 - 15 */ } DATUM; typedef union { DATUM datum; unsigned int cislo; } BITY; int main(void) { BITY dnes; int den, mesic, rok; printf(\"Zadej dnesni datum [dd mm rrrr]: \"); scanf(\"%d %d %d\", &amp;den, &amp;mesic, &amp;rok); dnes.cislo = 0; printf(\"datum: %2d.%2d.%4d - cislo: %04X hexa\\n\", dnes.datum.den, dnes.datum.mesic, dnes.datum.rok + 1980, dnes.cislo); dnes.datum.den = den; printf(\"datum: %2d.%2d.%4d - cislo: %04X hexa\\n\", dnes.datum.den, dnes.datum.mesic, dnes.datum.rok + 1980, dnes.cislo); dnes.datum.mesic = mesic; printf(\"datum: %2d.%2d.%4d - cislo: %04X hexa\\n\", dnes.datum.den, dnes.datum.mesic, dnes.datum.rok + 1980, dnes.cislo); dnes.datum.rok = rok - 1980; printf(\"datum: %2d.%2d.%4d - cislo: %04X hexa\\n\", dnes.datum.den, dnes.datum.mesic, dnes.datum.rok + 1980, dnes.cislo); return 0; }</pre><p> </p> </div>"
+
+    @newtext = CGI.escape(@text)
   end
 
 end
